7bac7f5293db5d188d001dce94e056ed5dbd35e7
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-08-15 21:09:36 +0500

    Added test names and descriptions for the Allure report

--- src/test/java/com/dimqa/tests/IngredientsTabsTest.java
+++ src/test/java/com/dimqa/tests/IngredientsTabsTest.java
@@ -1,11 +1,15 @@
 package com.dimqa.tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IngredientsTabsTest extends TestBase {
 
     @Test
+    @DisplayName("Switching between tabs")
+    @Description("Switching between burger ingredient tabs: buns, sauces, fillings")
     public void switchingBetweenTabs() {
         homePage().clickSaucesTab();
         Assert.assertTrue(homePage().getSaucesTabAttributes().contains("tab_tab_type_current"));
